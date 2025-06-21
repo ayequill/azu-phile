dbc1838d29f2f2daaba5bb4e9fdd653add3354da
Siaw A. Nicholas <siawnic.dev@gmail.com>
2025-06-21 00:39:51 +0000

    Update product highlight components for YX1 and ZX9: adjust layout, improve responsiveness, and refine background image handling.

--- components/home/product-highlight-zx9.tsx
+++ components/home/product-highlight-zx9.tsx
@@ -1,8 +1,8 @@
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function ProductHighlight() {
   return (
-    <section className="lg:py-36 px-4 md:px-6 lg:px-8">
+    <section className="lg:py- px-4 md:px-6 lg:px-8">
       <div className="bg-[#D87D4A] max-w-7xl mx-auto my-2 rounded-lg font-manrope">
         <div className="lg:px-8 min-h-[700px] md:min-h-[700px] lg:min-h-[560px] bg-[url('/assets/home/desktop/pattern-circles.svg')] lg:bg-size-[944px_944px] bg-size-[500px_500px] lg:bg-position-[top_20%_left_-40%] bg-position-[top_-70%_center] bg-no-repeat grid grid-cols-1 lg:grid-cols-2">
           <div className="bg-[url('/assets/home/desktop/image-speaker-zx9.png')] bg-size-[auto_200px] lg:bg-size-[auto_410px] bg-position-[bottom_-10%_center] bg-no-repeat" />
